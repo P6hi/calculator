109be50b9c9c819bab08cd9ab0fcdba2a52f7271
Arthur <a-murk@mail.ee>
2022-05-27 17:30:30 +0300

    Create operate function

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function multiplication(a,b) {
 function division(a,b) {
     return a / b;
 }
+
+function operate(a, o, b) {
+    if (o == "+") {
+        addition(a,b);
+    } else if (o == "-") {
+        subtraction(a,b);
+    } else if (o == "*") {
+        multiplication(a,b);
+    } else if (o == "/") {
+        division(a,b);
+    }
+}
