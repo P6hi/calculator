4d4bd1644412afa028e41ffacb034c3c680928ca
Arthur <a-murk@mail.ee>
2022-05-27 22:38:09 +0300

    Delete problematic code
    Code was delete for a fresh start and for separate branch creation

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ function multiplication(a,b) {
 }
 
 function division(a,b) {
-    return a / b;
+    let result = a / b;
+    display.textContent = '';
+    display.textContent += result;
 }
 
 function operate(a, o, b) {
@@ -19,9 +21,9 @@ function operate(a, o, b) {
         addition(a,b);
     } else if (o == "-") {
         subtraction(a,b);
-    } else if (o == "*") {
+    } else if (o == "×") {
         multiplication(a,b);
-    } else if (o == "/") {
+    } else if (o == "÷") {
         division(a,b);
     }
 }
@@ -30,35 +32,13 @@ const buttons = document.querySelectorAll(".button-number");
 const display = document.querySelector(".display");
 const clear = document.querySelector('#clear');
 const operators = document.querySelectorAll('.operator');
-let displayValue = display.textContent;
+const equals = document.querySelector('#equal');
 const digits = {
-    number1: displayValue,
+    number1: null,
     operator: null,
-    number2: 0
+    number2: null
 }
 
-operators.forEach(operator => {
-    operator.addEventListener('click', (e) => {
-        display.textContent += (` ${e.target.textContent}`);
-        //if number 1 = empty add displayValue to digits.number1
-        //if not empty = add displayValue to digits.number2
-        //run operate function
-        digits.operator = e.target.textContent;
-        digits.number1 = displayValue;
-    })
-})
-
-buttons.forEach(button => {
-    button.addEventListener('click', (e) => {
-        let toDisplay = e.target.textContent;
-        display.textContent += toDisplay;
-        displayValue += toDisplay;
-    })
-})
-
-clear.addEventListener('click', () => {
-    display.textContent = '';
-    displayValue = '';
-    digits.number1 = null;
-    digits.operator = null;
-})
+// Numbers added to display get an HTML id with their value
+// Value is used when adding to number1 and number 2
+// After operator button is pushed, if number1 has a value, the next number is added with an id of the value, and based on that value it is added to number2
