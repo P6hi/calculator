b88f0bfb2cc44022622d61c437bb7520498175bc
Arthur <a-murk@mail.ee>
2022-05-27 23:42:01 +0300

    Create button functionality
    Pressing number buttons assigns them as a property value in the digits object, operators are saved when pressed too. Equals button runs the operation and shows the result, saving it in the number1 property of the digits object

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
 function addition(a,b) {
-    return a + b;
+    let result =  +a + +b;
+    display.textContent = '';
+    display.textContent += result;
+    digits.number1 = result;
+    digits.operator = null;
+    digits.number2 = null;
 }
 
 function subtraction(a,b) {
-    return a - b;
+    let result = +a - +b;
+    display.textContent = '';
+    display.textContent += result;
+    digits.number1 = result;
+    digits.operator = null;
+    digits.number2 = null;
 }
 
 function multiplication(a,b) {
-    return a * b;
+    let result = +a * +b;
+    display.textContent = '';
+    display.textContent += result;
+    digits.number1 = result;
+    digits.operator = null;
+    digits.number2 = null;
 }
 
 function division(a,b) {
-    let result = a / b;
-    display.textContent = '';
-    display.textContent += result;
+    if (a == 0 || b == 0) {
+        display.textContent = '';
+        display.textContent += "Don't do that.";
+    } else {
+        let result = a / b;
+        display.textContent = '';
+        display.textContent += result;
+        digits.number1 = result;
+        digits.operator = null;
+        digits.number2 = null;
+    }
 }
 
 function operate(a, o, b) {
-    if (o == "+") {
+    if (o === "+") {
         addition(a,b);
-    } else if (o == "-") {
+    } else if (o === "-") {
         subtraction(a,b);
-    } else if (o == "×") {
+    } else if (o === "×") {
         multiplication(a,b);
-    } else if (o == "÷") {
+    } else if (o === "÷") {
         division(a,b);
     }
 }
@@ -39,6 +62,48 @@ const digits = {
     number2: null
 }
 
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (digits.number1 !== null && digits.number2 === null) {
+            digits.number2 = '';
+            display.textContent = '';
+            display.textContent += e.target.textContent;
+            digits.number2 += e.target.textContent;
+        } else if (digits.number1 !== null && digits.number2 !== null) {
+            digits.number2 += e.target.textContent;
+            display.textContent += e.target.textContent;
+        } else {
+            display.textContent += e.target.textContent;
+        }
+    })
+})
+
+clear.addEventListener('click', () => {
+    display.textContent = '';
+    digits.number1 = null;
+    digits.operator = null;
+    digits.number2 = null;
+})
+
+operators.forEach(operator => {
+    operator.addEventListener('click', (e) => {
+        if (digits.number1 === null) {
+            digits.number1 = display.textContent;
+            digits.operator = e.target.textContent;
+            display.textContent += (' ' + e.target.textContent);
+        } else if (digits.number1 !== null && digits.operator === null) {
+            digits.operator = e.target.textContent;
+            display.textContent += (' ' + e.target.textContent);
+        }
+        console.log(digits);
+    })
+})
+
+equals.addEventListener('click', () => {
+    if (digits.number1 !== null && digits.number2 !== null && digits.operator !== null) {
+        operate(digits.number1, digits.operator, digits.number2);
+    }
+})
 // Numbers added to display get an HTML id with their value
 // Value is used when adding to number1 and number 2
 // After operator button is pushed, if number1 has a value, the next number is added with an id of the value, and based on that value it is added to number2
