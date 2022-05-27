79bbb735478bd26f156df127482c1e9f88ddec25
Arthur <a-murk@mail.ee>
2022-05-27 22:08:34 +0300

    Add calculator divs
    Create calculator structure with divs and add classes the button divs

--- script.js
+++ script.js
@@ -25,3 +25,40 @@ function operate(a, o, b) {
         division(a,b);
     }
 }
+
+const buttons = document.querySelectorAll(".button-number");
+const display = document.querySelector(".display");
+const clear = document.querySelector('#clear');
+const operators = document.querySelectorAll('.operator');
+let displayValue = display.textContent;
+const digits = {
+    number1: displayValue,
+    operator: null,
+    number2: 0
+}
+
+operators.forEach(operator => {
+    operator.addEventListener('click', (e) => {
+        display.textContent += (` ${e.target.textContent}`);
+        //if number 1 = empty add displayValue to digits.number1
+        //if not empty = add displayValue to digits.number2
+        //run operate function
+        digits.operator = e.target.textContent;
+        digits.number1 = displayValue;
+    })
+})
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        let toDisplay = e.target.textContent;
+        display.textContent += toDisplay;
+        displayValue += toDisplay;
+    })
+})
+
+clear.addEventListener('click', () => {
+    display.textContent = '';
+    displayValue = '';
+    digits.number1 = null;
+    digits.operator = null;
+})
